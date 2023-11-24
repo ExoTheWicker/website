7368e3468b555a43ba9ea072712eaa208efb1cda
ExoTheWicker <70044982+ExoTheWicker@users.noreply.github.com>
2023-11-24 01:14:59 -0500

    Began adding blog
    began implementing blog system

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.set('views' , 'public')
 const port = 3621
+const db = require('./db/db');
+const blogRoutes = require('./routes/blog');
 
+// Use the blog routes
+app.use('/blog', blogRoutes);
+// Middleware to pass links data to every view
+app.use((req, res, next) => {
+  // Assuming links.getGroupedLinks is asynchronous and takes a callback
+  links.getGroupedLinks((err, groupedLinks) => {
+    if (err) {
+      console.error('Error getting grouped links:', err);
+      return next(err);
+    }
+    res.locals.groupedLinks = groupedLinks;
+    next();
+  });
+});
 
 app.get('/', (req, res) => {
   links.getGroupedLinks((err, groupedLinks) => {
