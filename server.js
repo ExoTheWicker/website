8dcd1b1208dc93e602efcae0acf094898d341f82
ExoTheWicker <70044982+ExoTheWicker@users.noreply.github.com>
2024-05-05 03:26:52 -0400

    .

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+var colors = require('colors');
 
 const app = express()
 const serveIndex = require('serve-index')
@@ -23,7 +24,15 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.use('/ftp', express.static('public/files'), serveIndex('public/files', {'icons': true, template: 'public/views/ftp.html'}))
+app.use('/files', express.static('public/files'), serveIndex('public/files', {'icons': true}))
+
+app.get('/liny', (req, res) => {
+    res.render('liny')
+})
 
 app.listen(process.env.PORT)
-console.log('Starting ExoTheWicker website Server on port ' + process.env.PORT)
+console.log('Starting ExoWebOS...' .yellow)
+console.log('Connecting to SQL database...')
+console.log('Checking SkyAura API for status...')
+console.log('checking WickerAPI status...')
+console.log('All checks ' + 'OK! '.green + ' starting server on port '.brightGreen + process.env.PORT .brightGreen)
